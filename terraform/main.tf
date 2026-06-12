0bf9b16b2791943d488bde4cc2a1c12e428443e1
LunarVim <36399805+Messier333@users.noreply.github.com>
2026-06-12 17:10:44 +0900

    Increase dedicated memory from 8000 to 12000

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,7 +60,7 @@ resource "proxmox_virtual_environment_container" "k3s_master" {
   }
 
   memory {
-    dedicated = 8000
+    dedicated = 12000
     swap      = 0
   }
 
@@ -116,7 +116,7 @@ resource "proxmox_virtual_environment_container" "k3s_worker_1" {
   }
 
   memory {
-    dedicated = 8000
+    dedicated = 12000
     swap      = 0
   }
 
